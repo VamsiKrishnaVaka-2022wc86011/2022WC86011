4aba6fcf85f5846d3056122d739bb710ee972f06
VamsiKrishnaVaka-2022wc86011 <2022wc86011@wilp.bits-pilani.ac.in>
2026-09-25 02:15:53 +0530

    Modifed source code for CI

--- src/main/java/com/devops/HelloWorld.java
+++ src/main/java/com/devops/HelloWorld.java
@@ -3,5 +3,6 @@ package com.devops;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, this is my DevOps Lab project.");
+        System.out.println("My project Continuous Integration.");
     }
 }
